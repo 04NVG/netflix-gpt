26d90a9cf39ca1140cf7d6daddaba27c85f35120
maxim2025 <shosafoevmaksim@gmail.com>
2026-01-10 10:58:57 +0500

    Add login form changes

---
+++ src/components/Login.js
@@ -0,0 +1,88 @@
+import React, { useRef, useState } from "react";
+import { Header } from "./Header";
+import { checkValidData } from "../utils/validate";
+
+ const Login = () => {
+  const [isSignInForm, setisSignInForm] = useState(true);
+  const [errorMessage,seterrorMessage] = useState(null)
+
+  const email = useRef(null);
+  const password = useRef(null);
+  const fullname= useRef(null)
+
+  const handleButtonClick = () => {
+
+     
+    const fullNameValue = fullname.current
+    ? fullname.current.value
+    : "";
+
+  const mes = checkValidData(
+    email.current.value,
+    password.current.value,
+    fullNameValue
+  );
+
+  seterrorMessage(mes);
+};
+
+  const toggleSignInForm = () => {
+    setisSignInForm(!isSignInForm);
+  };
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
+      <Header />
+      <div className="absolute inset-0 -z-10">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/e393bb3f-261f-43d1-99bb-16a157885615/web/TJ-ru-20260105-TRIFECTA-perspective_fa6ece3f-a5be-4ce0-a137-3d863457a489_large.jpg"
+          alt="phot"
+              className="w-full h-full object-cover"
+        />
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="bg-black bg-opacity-80 text-white rounded-lg p-8 sm:p-12
+                 w-11/12 max-w-md mx-auto my-52 flex flex-col">
+      
+        <h1 className="text-2xl sm:text-3xl font-bold mb-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+           ref={fullname}
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-2 w-full bg-gray-700"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-2 my-2 w-full bg-gray-700"
+        />
+        
+
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-2 my-2 w-full bg-gray-700"
+        />
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <button
+         className="bg-red-700 py-3 rounded w-full my-4 hover:bg-red-800 transition"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now..."}
+        </p>
+      </form>
+    </div>
+  );
+};
+export default Login
