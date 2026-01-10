6f35e5533340adefa51333b8096b622ce95c3b35
maxim2025 <shosafoevmaksim@gmail.com>
2026-01-10 16:39:59 +0500

    registration

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,74 @@
 import React, { useRef, useState } from "react";
 import { Header } from "./Header";
 import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
- const Login = () => {
+const Login = () => {
   const [isSignInForm, setisSignInForm] = useState(true);
-  const [errorMessage,seterrorMessage] = useState(null)
+  const [errorMessage, seterrorMessage] = useState(null);
 
   const email = useRef(null);
   const password = useRef(null);
-  const fullname= useRef(null)
+  const fullname = useRef(null);
+  const navigate = useNavigate();
 
   const handleButtonClick = () => {
+    const fullNameValue = fullname.current ? fullname.current.value : "";
 
-     
-    const fullNameValue = fullname.current
-    ? fullname.current.value
-    : "";
+    const mes = checkValidData(
+      email.current.value,
+      password.current.value,
+      fullNameValue
+    );
 
-  const mes = checkValidData(
-    email.current.value,
-    password.current.value,
-    fullNameValue
-  );
+    seterrorMessage(mes);
+    if (mes) return;
+    //
+    if (!isSignInForm) {
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
 
-  seterrorMessage(mes);
-};
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          seterrorMessage(errorCode, errorMessage);
+          // ..
+        });
+    } else {
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          seterrorMessage(errorCode, errorMessage);
+        });
+    }
+  };
 
   const toggleSignInForm = () => {
     setisSignInForm(!isSignInForm);
@@ -36,20 +80,20 @@ import { checkValidData } from "../utils/validate";
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/e393bb3f-261f-43d1-99bb-16a157885615/web/TJ-ru-20260105-TRIFECTA-perspective_fa6ece3f-a5be-4ce0-a137-3d863457a489_large.jpg"
           alt="phot"
-              className="w-full h-full object-cover"
+          className="w-full h-full object-cover"
         />
       </div>
       <form
         onSubmit={(e) => e.preventDefault()}
         className="bg-black bg-opacity-80 text-white rounded-lg p-8 sm:p-12
-                 w-11/12 max-w-md mx-auto my-52 flex flex-col">
-      
+                 w-11/12 max-w-md mx-auto my-52 flex flex-col"
+      >
         <h1 className="text-2xl sm:text-3xl font-bold mb-4">
           {isSignInForm ? "Sign In Bro" : "Sign Up"}
         </h1>
         {!isSignInForm && (
           <input
-           ref={fullname}
+            ref={fullname}
             type="text"
             placeholder="Full Name"
             className="p-2 my-2 w-full bg-gray-700"
@@ -61,7 +105,6 @@ import { checkValidData } from "../utils/validate";
           placeholder="Email Address"
           className="p-2 my-2 w-full bg-gray-700"
         />
-        
 
         <input
           ref={password}
@@ -71,7 +114,7 @@ import { checkValidData } from "../utils/validate";
         />
         <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
         <button
-         className="bg-red-700 py-3 rounded w-full my-4 hover:bg-red-800 transition"
+          className="bg-red-700 py-3 rounded w-full my-4 hover:bg-red-800 transition"
           onClick={handleButtonClick}
         >
           {isSignInForm ? "Sign In" : "Sign Up"}
@@ -85,4 +128,4 @@ import { checkValidData } from "../utils/validate";
     </div>
   );
 };
-export default Login
+export default Login;
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,16 +1,19 @@
-export const checkValidData = (email, password, firstName) => {
+export const checkValidData = (email, password, fullName) => {
   const isEmailValid = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
 
   const isPasswordValid =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-  const isFullNamevalid =
-    /(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})/.test(
-      firstName
-    );
+
+   if (fullName && fullName.trim() !== "") {
+    if (!/^[a-zA-Zа-яА-ЯёЁ\s]{2,50}$/.test(fullName)) {
+      return "Full name is not valid";
+    }
+  }
+  
 
   if (!isEmailValid) return "Email ID is not valid";
   if (!isPasswordValid) return "Passsword is not  valid";
-  if (!isFullNamevalid) return "Firstname is not  valid";
+ 
 
   return null;
   
