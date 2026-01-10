e3436b92b2e6f50b267c601850b3c8ac2e706c01
maxim2025 <shosafoevmaksim@gmail.com>
2026-01-10 16:45:14 +0500

    done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Login from "./Login";
 import { Browse } from "./Browse";
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import { createBrowserRouter, RouterProvider,  } from "react-router-dom";
 import {  onAuthStateChanged } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { useDispatch } from "react-redux";
@@ -36,7 +36,7 @@ export const Body = () => {
   }
 });
 
-  },[])
+  }, [dispatch])
 
 
 
