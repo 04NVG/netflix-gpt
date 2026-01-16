c7c633238fa889a85b37673c4d35d09bcbca96cf
maxim2025 <shosafoevmaksim@gmail.com>
2026-01-16 17:12:20 +0500

    Add Moveis List

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 export const SecondaryContainer = () => {
+  const movies = useSelector((store) => store.movies);
+  console.log(movies)
   return (
-    <div>SecondaryContainer</div>
-  )
-}
+    <div>
+      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
+    </div>
+  );
+};
