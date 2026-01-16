1fb92f278d28db128ad31afebafd7f211d49865f
maxim2025 <shosafoevmaksim@gmail.com>
2026-01-16 15:27:40 +0500

    Fetch and add main

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,42 +1,22 @@
-import React, { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import {addTrailerVideo}  from "../utils/moviesSlice"
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 export const VideoBackground = ({ movieId }) => {
-  const trailerVideo = useSelector((store) => store.movies.trailerVideo)
-   const dispatch = useDispatch()
-
-
-  const getTrailerVideo = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/83533/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log("maksim", json);
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    console.log(trailer);
-    dispatch(addTrailerVideo((trailer)))
-  };
-
-  useEffect(() => {
-    getTrailerVideo();
-  }, [movieId, dispatch]);
-
+  const trailerVideo = useSelector((store) => store.movies.trailerVideo);
+  useMovieTrailer(movieId);
   return (
-    <div>
+    <div className="">
       <iframe
-        width="560"
-        height="315"
-        src={"https://www.youtube.com/embed/"+trailerVideo?.key}
+        className="w-screen aspect-video"
+        src={
+          "https://www.youtube.com/embed/" +
+          trailerVideo?.key +
+          "?&autoplay=1&mute=1"
+        }
         title="YouTube video player"
-        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      
       ></iframe>
     </div>
   );
 };
+ 
