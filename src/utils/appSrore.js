55909dc62b8960191383e9bb7bce8ef610a49232
maxim2025 <shosafoevmaksim@gmail.com>
2026-01-19 11:58:18 +0500

    Starting add Api from GPT

--- src/utils/appSrore.js
+++ src/utils/appSrore.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  userReducer from "../utils/userSlice"
 import  moviesreducer from "./moviesSlice"
+import gptReducer from "./gptSlice"
 
 
 const appStore = configureStore({
   reducer: {
     user: userReducer,
     movies: moviesreducer,
+    gpt: gptReducer
   },
 });
 
