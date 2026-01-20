aad2f8435e39a9ed3d4b7de92cb7e2b6222a982c
maxim2025 <shosafoevmaksim@gmail.com>
2026-01-20 15:49:01 +0500

    fixed

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,26 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import { API_OPTIONS } from "../utils/constants";
 import { addTrailerVideo } from "../utils/moviesSlice";
 import { useDispatch } from "react-redux";
 
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
-  const getTrailerVideo = async () => {
+
+  const getTrailerVideo = useCallback(async () => {
+    if (!movieId) return;
+
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/" + movieId +"/videos?language=en-US",
+      "https://api.themoviedb.org/3/movie/" + movieId + "/videos?language=en-US",
       API_OPTIONS
     );
     const json = await data.json();
-   
 
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-  
+    const filterData = json.results.filter(
+      (video) => video.type === "Trailer"
+    );
+    const trailer = filterData.length
+      ? filterData[0]
+      : json.results[0];
+
     dispatch(addTrailerVideo(trailer));
-  };
+  }, [dispatch, movieId]);
 
   useEffect(() => {
     getTrailerVideo();
-  }, [movieId]);
+  }, [getTrailerVideo]);
 };
+
 export default useMovieTrailer;
