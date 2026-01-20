9b99b9f465f3dd95b6d9c9f147c40395b06b3dab
maxim2025 <shosafoevmaksim@gmail.com>
2026-01-20 15:42:51 +0500

    done

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import { API_OPTIONS } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
@@ -7,23 +7,23 @@ const useNowPlayingMovies = () => {
   const dispatch = useDispatch();
 
   const nowPlayingMovies = useSelector(
-    (store) => store.movies.nowPlayingMovies,
+    (store) => store.movies.nowPlayingMovies
   );
 
-  const getNowPlayingMovies = async () => {
+  const getNowPlayingMovies = useCallback(async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS,
+      API_OPTIONS
     );
     const json = await data.json();
-
     dispatch(addNowPlayingMovies(json.results));
-  };
+  }, [dispatch]);
 
   useEffect(() => {
-    if(!nowPlayingMovies)
-    getNowPlayingMovies();
-  }, []);
+    if (!nowPlayingMovies) {
+      getNowPlayingMovies();
+    }
+  }, [nowPlayingMovies, getNowPlayingMovies]);
 };
 
 export default useNowPlayingMovies;
